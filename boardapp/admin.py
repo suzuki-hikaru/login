7c5a5f14646f1de07982578dcf35f7f8bba74431
hikaru <umisuki20@gmail.com>
2020-06-23 22:13:44 +0900

    追加

---
+++ boardapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Boardmodel
+
+# Register your models here.
+
+admin.site.register(Boardmodel)
---
+++ boardapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Boardmodel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='')
+    good = models.IntegerField()
+    read = models.IntegerField()
+    readtext = models.CharField(max_length=100)
+
+
